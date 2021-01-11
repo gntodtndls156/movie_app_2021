a7c0d42527b5916124d61ca504e2de4f4c151a75
gntodtndls156 <gntodtndls156@gmail.com>
2021-01-11 21:54:33 +0900

    To install Prep-Types and how to use.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import propTypes from "prop-types";
 
 const foodILike = [
   {
@@ -6,50 +7,66 @@ const foodILike = [
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 4.9,
   },
   {
     id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9,
   },
   {
     id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.8,
   },
   {
     id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5.5,
   },
   {
     id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.7,
   },
 ];
 
-function Food({ favourite, image, name }) {
+function Food({ favourite, image, name, rating }) {
   return (
     <div>
       <h3>I love {favourite}</h3> <br />
+      <h4>{rating}/5.0</h4> <br />
       <img src={image} alt={name} />
     </div>
   );
 }
 
-function renderFood(item) {
-  return <Food key={item.id} favourite={item.name} image={item.image} name={item.name}/>
-}
+Food.propTypes = {
+  name: propTypes.string.isRequired,
+  image: propTypes.string.isRequired,
+  rating: propTypes.number.isRequired,
+};
 
 function App() {
   return (
     <div className="App">
-      {foodILike.map(renderFood)}
+      {foodILike.map((item) => (
+        <Food
+          key={item.id}
+          rating={item.rating}
+          favourite={item.name}
+          image={item.image}
+          name={item.name}
+        />
+      ))}
     </div>
   );
 }
